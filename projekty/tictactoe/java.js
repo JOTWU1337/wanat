8f888c7dbca67d068b21d695787f109aed32a888
JOTWU <165418080+JOTWU1337@users.noreply.github.com>
2026-01-15 19:07:27 +0100

    Update java.js

--- projekty/tictactoe/java.js
+++ projekty/tictactoe/java.js
@@ -1,9 +1,14 @@
 function sprawdz(){
-    let osoba1 = document.getElementById("osoba1").value;
-    let osoba2 = document.getElementById("osoba2").value;
+    let osoba1 = document.getElementById("osoba1").value.trim().toLowerCase();
+    let osoba2 = document.getElementById("osoba2").value.trim().toLowerCase();
+
     let wynik = document.getElementById("wynik");
-    if((osoba1=="Natalia" && osoba2=="Nicolas") || (osoba1=="Nicolas" && osoba2=="Natalia")){
-        wynik.innerHTML = "100% ❤️";
+
+    if(
+        (osoba1 === "natalia" && osoba2 === "nicolas") ||
+        (osoba1 === "nicolas" && osoba2 === "natalia")
+    ){
+        wynik.innerHTML = "100% ❤️ Pewniak";
     } else {
         let procent = Math.floor(Math.random() * 101);
         wynik.innerHTML = procent + "% 🤔";
