936faa530ac242467e64043c3611965f772e21b4
JOTWU <165418080+JOTWU1337@users.noreply.github.com>
2026-01-15 19:17:51 +0100

    Add love percentage message for Nicolas and Paulina

--- projekty/tictactoe/java.js
+++ projekty/tictactoe/java.js
@@ -10,6 +10,8 @@ function sprawdz(){
         wynik.innerHTML = "101% stara miłość nie rdzewieje";
     }else if((osoba1 === "szymon" && osoba2 === "nadia") || (osoba1 === "nadia" && osoba2 === "szymon")){
         wynik.innerHTML = "99% ❤️";
+    }else if((osoba1 === "nicolas" && osoba2 === "paulina") || (osoba1 === "paulina" && osoba2 === "nicolas")){
+        wynik.innerHTML = "100% milfy najlepsze";
     } else {
         let procent = Math.floor(Math.random() * 101);
         wynik.innerHTML = procent + "% 🤔";
