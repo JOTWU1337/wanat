c3e007610245243f668dce3e7fcc9a1fc012852f
JOTWU <165418080+JOTWU1337@users.noreply.github.com>
2026-01-15 19:13:12 +0100

    Update java.js

--- projekty/tictactoe/java.js
+++ projekty/tictactoe/java.js
@@ -4,18 +4,11 @@ function sprawdz(){
 
     let wynik = document.getElementById("wynik");
 
-    if(
-        (osoba1 === "natalia" && osoba2 === "nicolas") ||
-        (osoba1 === "nicolas" && osoba2 === "natalia")
-    ){
+    if((osoba1 === "natalia" && osoba2 === "nicolas") || (osoba1 === "nicolas" && osoba2 === "natalia")){
         wynik.innerHTML = "100% ❤️ Pewniak";
-    } else if(
-        (osoba1 === "natalia" && osoba2 === "alan") ||
-        (osoba1 === "alan" && osoba2 === "natalia")
-    ){
-        wynik.innerHTML = "0% nie ma co sie brać za to";
-    }
-    else {
+    } else if((osoba1 === "natalia" && osoba2 === "alan") || (osoba1 === "alan" && osoba2 === "natalia")){
+        wynik.innerHTML = "101% stara miłość nie rdzewieje";
+    } else {
         let procent = Math.floor(Math.random() * 101);
         wynik.innerHTML = procent + "% 🤔";
     }
